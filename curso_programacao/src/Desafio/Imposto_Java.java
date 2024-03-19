3dcb992049426cd19545be309fc21e3b198de855
Luan Araujo <69086390+luandev001@users.noreply.github.com>
2024-03-18 21:05:28 -0300

    Project structure, data entry

--- curso_programacao/src/Desafio/Imposto_Java.java
+++ curso_programacao/src/Desafio/Imposto_Java.java
@@ -1,10 +1,34 @@
 package Desafio;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Imposto_Java {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Renda anual com salário: ");
+		double incomeAnnual = sc.nextDouble(); //Renda anual
+		
+		System.out.println("Renda anual com prestação de serviço: ");
+		double serviceProvision = sc.nextDouble();//pretação de serviço
+		
+		System.out.println("Renda anual com ganho de capital: ");
+		double capitalGain = sc.nextDouble();  // Ganho de capital
+		
+		System.out.println("Gastos médicos: ");
+		double medicalExpenses = sc.nextDouble();  // Gastos médico
+		
+		System.out.println("Gastos educacionais: ");
+		double educationalExpenses = sc.nextDouble();  // Gastos educacionais
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
