363c5ae836c9ad44d685e5fe995c12c6a98d4d2c
Luan Araujo <69086390+luandev001@users.noreply.github.com>
2024-07-22 21:55:38 -0300

    Encapsulamento - adding getters e setters

--- stock_product/src/application/Program.java
+++ stock_product/src/application/Program.java
@@ -21,6 +21,11 @@ public class Program {
 		
 		Product product = new Product(name, price);
 		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
+		
 		System.out.println();
 		System.out.println("Product data: " + product);
 		
