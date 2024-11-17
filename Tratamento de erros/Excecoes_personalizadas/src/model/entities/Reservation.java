b6316cf2866ebed9ce94e1feb41c4b3060d019da
Luan Araujo <luan.araujo.santos10@gmail.com>
2024-11-17 15:08:38 -0300

    Excecoes Personalizadas - Very bad solution

---
+++ Tratamento de erros/Excecoes_personalizadas/src/model/entities/Reservation.java
@@ -0,0 +1,67 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date  checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");//Está como static para que não seja estanciado para cada objeto reservation que a aplicação tiver
+	
+	public Reservation() {
+	}
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime(); // PEgar diferença entre as duas datas
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); //Converter diff de milesegundos --> dias.
+				
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
+	}
+	
+	
+	
+	
+}
