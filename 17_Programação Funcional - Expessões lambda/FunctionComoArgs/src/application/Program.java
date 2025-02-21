9516002d8b72f9bcc0c39143e78932b56002fd52
Luan Araujo <luan.araujo.santos10@gmail.com>
2025-02-21 02:02:10 -0300

    FunctionArgs - Expressão lambda

--- 17_Programação Funcional - Expessões lambda/FunctionComoArgs/src/application/Program.java
+++ 17_Programação Funcional - Expessões lambda/FunctionComoArgs/src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		ProductService ps = new ProductService();
-		double sum = ps.filteredSum(list);
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
 		System.out.println("Sum = " + String.format("%.2f", sum));
 	}
 
