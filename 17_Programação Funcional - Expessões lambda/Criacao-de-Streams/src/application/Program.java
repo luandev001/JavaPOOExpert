fc6fd751495c9d58a19eb0ed09e1093b2bea6364
Luan Araujo <luan.araujo.santos10@gmail.com>
2025-07-01 06:05:36 -0300

    Criação de Streams

--- 17_Programação Funcional - Expessões lambda/Criacao-de-Streams/src/application/Program.java
+++ 17_Programação Funcional - Expessões lambda/Criacao-de-Streams/src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
 		
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
-		
+		/**
 		Stream<String> st2  = Stream.of("Maria", "Alex", "Bob");
 		System.out.println(Arrays.toString(st2.toArray()));
 		
@@ -21,6 +21,7 @@ public class Program {
 		
 		Stream<Long> st4 = Stream.iterate(new Long[]{0L, 1L}, p->new Long[]{ p[1], p[0]+p[1]}).map(p -> p[0]);
 		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+		*/
 	}
 
 }
